7896bc1b9c669ff06792296fafcd9db829429e5d
ChanZou <chanzou99@gmail.com>
2015-09-27 16:45:59 -0400

    MainBody Completed

---
+++ dbmutex.py
@@ -0,0 +1,35 @@
+from multiprocessing import Process, Lock
+import MainBody
+
+
+_author__ = 'Chan'
+
+
+class Client(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.db = MainBody.open(self.filename)
+        self.lock = Lock()
+
+    def Put(self, key, data):
+        key = str(key)
+        data = str(data)
+        # Meta data
+        self.db[key] = data
+        self.db.close()
+        self.db = MainBody.open(self.filename)
+
+    def Get(self, key):
+        key = str(key)
+        try:
+            # Meta data
+            return self.db[key]
+        finally:
+            self.db.close()
+            self.db = MainBody.open(self.filename)
+
+    def Remove(self, key):
+        key = str(key)
+        del self.db[key]
+        self.db.close()
+        self.db = MainBody.open(self.filename)
